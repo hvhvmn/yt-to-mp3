f9da9c6b970187dc9714e3d92a58a652376d468a
cricket112004@gmail.com <adheesh112004@gmail.com>
2026-03-20 11:23:22 +0530

    done

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,14 @@
 import express from "express";
 import ytRouter from "./routes/yt.routes.js";
 import cors from "cors"
+import path from "path"
 const server = express();
 server.use(cors({
     origin:"http://localhost:5173"
 }))
 server.use("/api/yt",ytRouter)
 server.use(express.static("./public"))
-app.get("*", (req, res) => {
+server.use((req, res) => {
     res.sendFile(path.resolve("backend/public/index.html"));
   });
 export default server;
